ba81c66f4f78cd96cd938386235aa469dcf57f30
PukanshuKumar <92430445+PukanshuKumar@users.noreply.github.com>
2025-04-26 23:55:50 +0530

    update app

---
+++ dist/mangas-web/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-KPD4SOD2.mjs";import"./chunk-L7UZB4HM.mjs";import"./chunk-VLNKBWKV.mjs";import"./chunk-BFCEQTNI.mjs";import"./chunk-HRXPSE3H.mjs";import"./chunk-K4MJOIOR.mjs";import"./chunk-7BJ56LUI.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
